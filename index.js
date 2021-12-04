310a163de0299a87a08906c898bbcfbc0be1d12b
Yuval Eliav <yuvaleliav1@gmail.com>
2021-12-04 03:08:53 +0200

    create dir if it isn't there

--- index.js
+++ index.js
@@ -33,11 +33,8 @@ app.post('/uploadFile', async function (req, res) {
 
     // Use the mv() method to place the file somewhere on your server
     try {
-        if (fs.existsSync(__dirname + '/images')) {
-            console.log("YES I EXIST BRAH")
-        } else {
-            console.log("NOOOO PLEASE BRAH")
-        }
+        !fs.existsSync(__dirname + '/images') && fs.mkdirSync(__dirname + '/images', { recursive: true })
+        
         fs.writeFileSync(uploadPath, sampleFile.data);
         console.log("File added")
     }
