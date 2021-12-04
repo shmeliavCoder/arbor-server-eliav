3c682e4e6e8b71d38da24e5544448a24afd1199c
Yuval Eliav <yuvaleliav1@gmail.com>
2021-12-04 14:33:19 +0200

    work with client instead of pool

--- dal.js
+++ dal.js
@@ -1,6 +1,6 @@
 const { Pool, Client } = require("pg");
 
-const pool = new Pool({
+const client = new Client({
   user: "postgres",
   host: "34.79.249.65",
   database: "postgres",
@@ -8,14 +8,19 @@ const pool = new Pool({
   port: "5432"
 });
 
-exports.insertImage = (imageName, date) => {
+exports.insertImage = async (imageName, date) => {
     const query = {
         text: 'INSERT INTO public."IMAGES"("imagePath", "dateToShow") VALUES ($1, $2);',
         values: [imageName, new Date(date)],
       }
-    return pool.query(
-        query
-      );
+    try {
+        const res = await client.query(query);
+        console.log('INSERT worked');
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        client.close();
+    }
 }
 
 exports.getImages = (imageName, date) => {
@@ -23,11 +28,12 @@ exports.getImages = (imageName, date) => {
       text: 'SELECT * FROM public."IMAGES" WHERE ',
       values: [imageName, new Date(date)],
     }
-  pool.query(
-      query,
-      (err, res) => {
-        console.log(err, res);
-        pool.end();
-      }
-    );
+    try {
+      const res = await client.query(query);
+      console.log('INSERT worked');
+  } catch (err) {
+      console.log(err.stack);
+  } finally {
+      client.close();
+  }
 }
